35b92b55d1fa975d11b7ad477b5c3ade62566d78
Sophia <sophieyawson12@gmail.com>
2024-05-26 20:00:20 +0000

    final commit, updated the readme and app.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={{ 
       flex: 1,
-      backgroundColor: "yellow", // Change background color here
+      backgroundColor: "yellow", 
       alignItems: "center",
       justifyContent: "center"}}>
       <Text style={{ fontSize: 24}}>My name is <Text style={{fontWeight: "bold"}}>Sophia Nhyira Yawson</Text></Text>
